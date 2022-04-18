72a4c2c5470252e44c32d840e1cb0cff268196df
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-18 15:54:33 +0300

    App-Name-Change

--- src/app/gitservice.service.ts
+++ src/app/gitservice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs';
+// import { Observable, throwError } from 'rxjs';
+// import { catchError, retry } from 'rxjs';
 // import { map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
